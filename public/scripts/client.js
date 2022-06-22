43b9a4362e1c3b99adc39f1a98fe5bbfb1823c67
ukflava <ukflava@gmail.com>
2022-06-22 16:07:51 -0700

    add POST, forms, add gap to main, loaded tweets obj

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -23,52 +23,68 @@ $(document).ready(function() {
       },
       "created_at": 1461113959088
     }
-  ]
+  ];
+  const loadTweets = () => {
+   
+    $.get(`/tweets`).then(data => {
+      // $('#00000').empty()
+      renderTweets(data);
+    
+    })
+  
+  }
+loadTweets()
 
-  $( "#targettweet" ).submit(function( event ) {
-    // alert( "Handler for .submit() called." );
-    // let postdata = $( "#targettweet" ).serialize();
-
+  $("#targettweet").submit(function(event) {
     event.preventDefault();
+    const $input = $('#tweet-text').val().length
+    let postdata = $.text("#targettweet").serialize();
+    if ($input < 1){ alert("too short")}
+    if ($input > 140){ alert("too long")}
+    // alert( "Handler for .submit() called." );
+    
+    else{ 
+    $.post( "/tweets", postdata)
+    .done(() => {
+
+      $("#remaining").html(remaining);
+      $('#tweet-text').val('').focus();
+      $('#tweetlist').empty();
+      loadTweets()
+    } )
+    
+  }
+  // console.log("postdata", postdata)
+  
   });
 
-const renderTweets = function(tweets) {
-  for (let tweet of tweets) { let buffer = createTweetElement(tweet);
-console.log("rendertweet:", tweet)
-     $('<article class="tweetbox">').html(buffer).prependTo('#tweetlist')
-}
+  const renderTweets = function(tweets) {
+    for (let tweet of tweets) {
+      let buffer = createTweetElement(tweet);
+      $('<article class="tweetbox">').html(buffer).prependTo('#tweetlist');
+    }
 
-  // loops through tweets
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
+     // $("#addTweet").on('click',()=>{
+    //   const $input = $("#targettweet")
+      //  const usertweet = $input.val()
+      //  const $buffertweet = $('<article>').text(usertweet).prependTo(data)
+      // $tweetlist.prepend($buffertweet)
+      // console.log($input);
+      //  $input.val('').focus()
+    // });
+  };
 
-
-  $("#addTweet").on('click',()=>{
-    const $input = $("#targettweet").serialize();
-  //  const usertweet = $input.val()
-  //  const $buffertweet = $('<article>').text(usertweet).prependTo(data)
-   // $tweetlist.prepend($buffertweet)
-   console.log($input)
-  //  $input.val('').focus()
-     })
-}
-
-const createTweetElement = function(tweet) {
-   let $tweet = `<div class="tweethead"><div><p><img src="${tweet.user['avatars']}width="20" height="20"">${tweet.user['name']}</p></div><p>${tweet.user['handle']}</p></div>
+  const createTweetElement = function(tweet) {
+    let $tweet = `<div class="tweethead"><div><p><img src="${tweet.user['avatars']}width="20" height="20"">${tweet.user['name']}</p></div><p>${tweet.user['handle']}</p></div>
           <p class="tweetbody">${tweet.content['text']} </p>
           
-          <div class="tweetfooter"> ${tweet.created_at}<div><i class="social fa-solid fa-flag"></i><i class="social fa-solid fa-retweet"></i><i class="social fa-solid fa-heart"></i> </div> </div>
-         
-  `
+          <div class="tweetfooter"> ${timeago.format(tweet.created_at)}<div><i class="social fa-solid fa-flag"></i><i class="social fa-solid fa-retweet"></i><i class="social fa-solid fa-heart"></i> </div> </div>
+    `
   
-  ;
- 
-  /* Your code for creating the tweet element */
-  // ...
-  return $tweet;
-}
+    return $tweet;
+  };
 
-renderTweets(data);
+  // renderTweets(data);
 
 });
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
 
-  $("#tweet-text").on('keyup', function(){
-    let limit = 140
+  $("#tweet-text").on('keyup', function() {
+    let limit = 140;
     const $input = $("#tweet-text");
-    const remaining = limit - $input.val().length
-    // if(remaining < 0){} 
-    color = remaining < 0 ?'red':'black'
+    const remaining = limit - $input.val().length;
+    // if(remaining < 0){}
+    color = remaining < 0 ? 'red' : 'black';
     $("#remaining").html(remaining);
     $("#remaining").css({
       'color': color
     });
-// const usertweet = $input.val()
-// const $buffertweet = $('<li>').text(usertweet).prependTo($tweetlist)
-// const$('tweetlength').prepend(usertweet)
-console.log(remaining)
-// $input.val('').focus()
-  })
+    // const usertweet = $input.val()
+    // const $buffertweet = $('<li>').text(usertweet).prependTo($tweetlist)
+    // const$('tweetlength').prepend(usertweet)
+    // console.log(remaining);
+    // $input.val('').focus()
+  });
   // $(".new-tweet").on('keypress',()=>{
   //   console.log("banana")
   // })
