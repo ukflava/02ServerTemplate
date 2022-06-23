f0fce5071ef6c95f877c91eebc8888d6fad22ad8
ukflava <ukflava@gmail.com>
2022-06-23 08:44:04 -0700

    add fuction for XSS safe text from tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,29 +1,4 @@
 $(document).ready(function() {
-  // const data = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": "https://i.imgur.com/73hZDYK.png"
-  //       ,
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
-  //       "handle": "@rd" },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   }
-  // ];
 
   const escape = function (str) {
     let div = document.createElement("div");
@@ -65,7 +40,6 @@ loadTweets()
     } )
     
   }
-  // console.log("postdata", postdata)
   
   });
   // loadTweets()
@@ -120,9 +94,9 @@ loadTweets()
   };
 
   const createTweetElement = function(tweet) {
+    
     let $tweet = `<div class="tweethead"><div><p><img src="${tweet.user['avatars']}width="20" height="20"">${tweet.user['name']}</p></div><p>${tweet.user['handle']}</p></div>
-          <p class="tweetbody">${tweet.content['text']} </p>
-          
+          <p class="tweetbody">${escape(tweet.content['text'])} </p>
           <div class="tweetfooter"> ${timeago.format(tweet.created_at)}<div><i class="social fa-solid fa-flag"></i><i class="social fa-solid fa-retweet"></i><i class="social fa-solid fa-heart"></i> </div> </div>
     `
   
